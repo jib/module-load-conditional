4a05deb2ca10d33e7f154d51257c13d9b3ef5b95
Jos Boumans <kane@cpan.org>
2007-09-16 10:01:46 +0000

    r4132@rimmer:  kane | 2007-09-15 15:34:40 +0200  * this be 0.18
    git-svn-id: http://oss.dwim.org/module-load-conditional@1958 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/Module/Load/Conditional.pm
+++ lib/Module/Load/Conditional.pm
@@ -18,7 +18,7 @@ BEGIN {
                         $FIND_VERSION $ERROR $CHECK_INC_HASH];
     use Exporter;
     @ISA            = qw[Exporter];
-    $VERSION        = '0.17_02';
+    $VERSION        = '0.18';
     $VERBOSE        = 0;
     $FIND_VERSION   = 1;
     $CHECK_INC_HASH = 0;
