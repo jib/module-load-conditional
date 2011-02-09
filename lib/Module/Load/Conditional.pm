0881e2323c5b55bc89c6f808ada075618cd2daad
Chris Williams <chris@bingosnet.co.uk>
2011-02-09 15:29:33 +0000

    Bump version to 0.42 and update CHANGES
    git-svn-id: http://oss.dwim.org/module-load-conditional@2905 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/Module/Load/Conditional.pm
+++ lib/Module/Load/Conditional.pm
@@ -18,7 +18,7 @@ BEGIN {
                         $FIND_VERSION $ERROR $CHECK_INC_HASH];
     use Exporter;
     @ISA            = qw[Exporter];
-    $VERSION        = '0.40';
+    $VERSION        = '0.42';
     $VERBOSE        = 0;
     $DEPRECATED     = 0;
     $FIND_VERSION   = 1;
