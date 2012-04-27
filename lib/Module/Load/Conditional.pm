a3737868b6860c0a92c3488dbf09ff2f347a1cf3
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2012-04-27 22:26:43 +0100

    Update CHANGES and bump to version 0.50

--- lib/Module/Load/Conditional.pm
+++ lib/Module/Load/Conditional.pm
@@ -18,7 +18,7 @@ BEGIN {
                         $FIND_VERSION $ERROR $CHECK_INC_HASH];
     use Exporter;
     @ISA            = qw[Exporter];
-    $VERSION        = '0.48';
+    $VERSION        = '0.50';
     $VERBOSE        = 0;
     $DEPRECATED     = 0;
     $FIND_VERSION   = 1;
